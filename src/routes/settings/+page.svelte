29b2296576db6fcdb5e3309d6cc29ca08383a2f9
Akira Ishino <akira@lumilab.jp>
2025-12-24 11:23:51 +0900

    settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import { Separator } from "$lib/components/ui/separator/index.js";
-  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-
   import Agent from "./Agent.svelte";
   import Core from "./Core.svelte";
 
@@ -12,16 +9,18 @@
   const agentDefs = $derived(data.agentDefs);
 </script>
 
-<main class="container mx-auto p-8 space-y-8 mt-20">
-  <header class="flex-none h-14 items-centger">
-    <Sidebar.Trigger />
-    <Separator orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
+<div class="w-full pl-4 pr-4 pb-6">
+  <header class="flex-none h-14 items-center">
+    <div class="text-2xl font-semibold">Settings</div>
   </header>
-  <h1 class="text-xl font-semibold sm:text-2xl">Settings</h1>
-  <Core {settings} />
+  <div class="@container/main flex flex-1 flex-col">
+    <Core {settings} />
 
-  <h2 class="text-xl font-semibold sm:text-2xl">Agents</h2>
-  {#each Object.entries(agentGlobalConfigsMap) as [agentName, agentConfigs]}
-    <Agent {agentName} {agentConfigs} agentDef={agentDefs[agentName]} />
-  {/each}
-</main>
+    <div class="flex flex-col mt-8 gap-6">
+      <div class="flex-none text-xl font-semibold">Agents</div>
+      {#each Object.entries(agentGlobalConfigsMap) as [agentName, agentConfigs]}
+        <Agent {agentName} {agentConfigs} agentDef={agentDefs[agentName]} />
+      {/each}
+    </div>
+  </div>
+</div>
